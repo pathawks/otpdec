1dd953e363a6c033b8a5c7567773ed53ee9b26f9
Pat Hawks <pat@pathawks.com>
2022-12-11 23:15:38 -0600

    Clean up with maps

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use sha1::Sha1;
 use std::time::SystemTime;
 use data_encoding::BASE32;
 use std::time::Duration;
+use std::ops::BitAnd;
 use std::io;
 
 static DIGITS_POWER: [u32; 9] = [1,10,100,1000,10000,100000,1000000,10000000,100000000];
@@ -11,8 +12,9 @@ static DIGITS_POWER: [u32; 9] = [1,10,100,1000,10000,100000,1000000,10000000,100
 fn get_current_time() -> u64 {
     return SystemTime::now()
         .duration_since(SystemTime::UNIX_EPOCH)
-        .unwrap_or(Duration::ZERO)
-        .as_secs();
+        .as_ref()
+        .map(Duration::as_secs)
+        .unwrap_or(0);
 }
 
 fn hex_string_to_bytes(hex: String) -> Vec<u8> {
@@ -32,13 +34,15 @@ fn hmac_sha<D>(key_bytes: Vec<u8>, text: Vec<u8>) -> Vec<u8> {
 
 fn generate_totp<D>(key: Vec<u8>, time: Vec<u8>, return_digits: usize) -> String {
     let hash = hmac_sha::<D>(key, time);
-    let offset_byte = 0xf & hash.last()
+    let offset = hash.last()
+        .as_deref()
+        .map(|b| b.bitand(0xf))
+        .map(usize::from)
         .expect("Hash needs to not be empty");
-    let offset = usize::from(offset_byte);
-    let bytes = hash[offset..offset+4]
+    let binary = 0x7fff_ffff & hash[offset..offset+4]
         .try_into()
+        .map(u32::from_be_bytes)
         .expect("Bytes");
-    let binary = u32::from_be_bytes(bytes) & 0x7fff_ffff;
     let otp = binary % DIGITS_POWER[return_digits];
     return format!("{:0digits$}", otp, digits = return_digits);
 }
